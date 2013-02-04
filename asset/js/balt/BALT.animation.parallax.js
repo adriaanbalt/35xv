d8ec2f6d00977eb237e40513b2d88aca7e9b6678
Adriaan Balt Louis Scholvinck <adriaan@BALT.us>
2013-02-03 19:04:12 -0500

    clearing out what is not necessary

--- asset/js/balt/BALT.animation.parallax.js
+++ asset/js/balt/BALT.animation.parallax.js
@@ -65,8 +65,8 @@ pass in an object that contains the following:
 		var hash = (window.location.hash==''?'#shop':window.location.hash);
 		settings.startAt = gotoSection[ hash ];
 		settings.endAt = $shop.height() + $accessories.height() + $share.height() + $display.height() + $performance.height() + $reviews.height();
-		
-		
+
+
 		//var scrollTopTweened += settings.tweenSpeed * (scrollTop - scrollTopTweened);
 
 		var init = function () {
@@ -77,7 +77,7 @@ pass in an object that contains the following:
 		/*
 		@description
 			function that is called for every pixel the user scrolls. Determines the position of the background
-		@param 
+		@param
 			x = horizontal position of background
 			windowHeight = height of the viewport
 			pos = position of the scrollbar
@@ -85,84 +85,8 @@ pass in an object that contains the following:
 			inertia = how fast the background moves in relation to scrolling
 		*/
 
-		var calculations = {
-			calcBgY : function(x, windowHeight, pos, adjuster, inertia){
-				return x + " " + (-((windowHeight + pos) - adjuster) * inertia)  + "px";
-			},
-			calcBgX : function(y, windowHeight, pos, adjuster, inertia){
-				return (-((windowHeight + pos) - adjuster) * inertia)  + "px " + y;
-			},
-			calcXY : function(windowHeight, pos, adjusterX, inertiaX, adjusterY, inertiaY){
-				return (-((windowHeight + pos) - adjusterX) * inertiaX)  + "px " + (-((windowHeight + pos) - adjusterY) * inertiaY) + "px";
-			},
-			calcPos : function(windowHeight, pos, adjuster, inertia) {
-				return (((windowHeight + pos) - adjuster) * inertia)  + "px ";
-			},
-			calcRot : function( r, windowHeight, pos, adjuster, inertia ){
-				return (r + -(((windowHeight + pos) - adjuster ) * inertia));
-			},
-			calcProgress : function( startAt, endAt ) {
-				progress = (startAt - scrollTopTweened) / (startAt - endAt);
-				return progress;
-			},
-			calcDegrees2Radians : function( degrees ) {
-				return ( degrees * Math.PI / 180 );
-			}
-		};
-
-		var spin = function() {
-			requestAnimFrame(spin);
-
-			scrollTopTweened += settings.tweenSpeed * ($window.scrollTop() - scrollTopTweened);
-			calcProgress( 10, settings.endAt );
-
-			var endFrame = (settings.imageCount/settings.skipImages) * settings.frameSpeed,
-			toFrame = Math.floor(progress*endFrame);
-			toFrame = (toFrame < 0 ? 0 : toFrame);
-			showImageAt( toFrame );
-
-			move();
-		};
-		
-		var showImageAt = function( index ) {
-			if (index == currentIndex) return false;
-			
-			var image = settings.images.children()[ index ];
-
-			if (image) {
-				hideImageAt( currentIndex );
-				currentIndex = index;
-				image.style.display = 'block';
-			} else {
-			}
-			
-		};
-
-		var hideImageAt = function( index ) {
-			var image = settings.images.children()[ index ];
-			if (image) image.style.display = 'none';	
-		};
-
 		var move = function(){
-			if ( ){
-				$spin.animate({
-					opacity : 1,
-					top: '10%'
-					}, 800, function() {
-				});
-				$shop.find('.copy-left').delay(250).animate({
-					opacity : 1,
-					top: '50px'
-					}, 500, function() {
-				});
-				$shop.find('.copy-right').delay(250).animate({
-					opacity : 1,
-					top: '50px'
-					}, 500, function() {
-					// done
-				});
-			}
-			
+
 			if($accessories.hasClass("inview")){
 				if ( pos < 700 ){
 					$accessories.find('.accessory-one').css({'background-position': calcBgX('65%', windowHeight, pos, 550, -0.3)});
@@ -172,13 +96,13 @@ pass in an object that contains the following:
 					$accessories.find('.accessory-three').css({'background-position': calcBgX('15%', windowHeight, pos, 2850, 0.7)});
 				}
 			}
-			
+
 			if( $share.hasClass("inview") && pos >= 850 && pos <= 1730 ){
 				$share.find('.wheel').css({ 'top': calcPos(windowHeight, pos, 1500, 0.2) });
 				$share.find('.wheel').css({ 'display': 'block' });
 				rotate = calcRot(55, windowHeight, pos, 1970, 0.5);
 				$share.find('.wheel').css(
-					{ 
+					{
 					'-ms-transform': 'rotate('+rotate+'deg)',
 					'-webkit-transform': 'rotate('+rotate+'deg)',
 					'transform': 'rotate('+rotate+'deg)',
@@ -244,52 +168,27 @@ pass in an object that contains the following:
 
 		var scrolling = function() {
 			pos = $window.scrollTop();
-			move();	
+			move();
 		};
 
 		var resize = function() {
 			move();
 		};
-		
+
 		var bindEvents = function(){
-			$cart.click( function(e){
-				if ( $(this).hasClass('is-open') ){
-					$(this).removeClass('is-open').addClass('is-closed');
-				} else if ( $(this).hasClass('is-closed') ){
-					$(this).removeClass('is-closed').addClass('is-open');
-				}
-			});
 			//apply the class "inview" to a section that is in the viewport
-			$('.section').bind('inview', function (event, visible) { 
+			$('.section').bind('inview', function (event, visible) {
 				if (visible == true) {
 					$(this).addClass("inview");
 				} else {
 					$(this).removeClass("inview");
 				}
 			});
-			$window.resize(resize);		
+			$window.resize(resize);
 			$window.bind('scroll', scrolling);
 			scrolling();
 			spin();
 		};
-
-		// animation loop
-		window.requestAnimFrame = (function(){
-			if (settings.useRAF) {
-				return  window.requestAnimationFrame || 
-				window.webkitRequestAnimationFrame   || 
-				window.mozRequestAnimationFrame      || 
-				window.oRequestAnimationFrame        || 
-				window.msRequestAnimationFrame       || 
-				function( callback ){
-					window.setTimeout(callback, settings.tickSpeed);
-				};
-			} else {
-				return function( callback ){
-					window.setTimeout( callback, settings.tickSpeed);
-				}
-			};
-		})();
 
 		init();
 	};
